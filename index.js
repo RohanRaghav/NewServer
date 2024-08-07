b4d2b23e82c5003642645d685ee9b0e8aaf725a2
Rohan Raghav <132052764+RohanRaghav@users.noreply.github.com>
2024-08-07 12:24:44 +0530

    first commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = 3001;
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const allowedOrigins = ['https://BootCamp.vercel.app', 'http://localhost:3000'];
+const allowedOrigins = ['https://boot-camp-topaz.vercel.app', 'http://localhost:3000'];
 
 app.use(cors({
   origin: function (origin, callback) {
